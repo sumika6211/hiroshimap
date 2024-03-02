7298b99b6294ad885dfbfe0773822ef610793430
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-03-02 12:22:58 +0000

    [Start]投稿機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
+  has_one_attached :post_image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :post_image, presence: true
+
 end
