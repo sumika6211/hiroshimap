4b8a38006bb99b52f57c4ddd06ffd0d953b85004
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-04-16 14:04:00 +0000

    [Finish]フォロー機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top], unless: :admin_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   if controller_name == "homes"
     before_action :ensure_guest_user, only: [:edit]
+  end
+
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! You have signed up successfully."
+    user_path(current_user.id)
+  end
+
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
+    user_path(current_user.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
+    root_path
   end
 
   private
@@ -17,4 +33,9 @@ class ApplicationController < ActionController::Base
       redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
