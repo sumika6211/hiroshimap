b51c2213c8878440944190078f31500c3c31a6da
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-04-24 11:58:05 +0000

    [Update]seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Admin.create!(
+  email: "admin@gmail.com",
+  password: "dndn6211",
+  password_confirmation: "dndn6211"
+)
+
 genres = %w(
   ランチ
   ディナー
