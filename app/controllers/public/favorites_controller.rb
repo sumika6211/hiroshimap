d1144bc133017d590832b31eb7f578e68b476961
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-06-05 14:34:50 +0000

    [Update]いいね機能コメント機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,15 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    current_user.favorites.find_or_create_by(post_id: post.id)
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    current_user.favorites.find_or_create_by(post_id: @post.id)
+    render 'public/favorites/button_toggle'
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy if favorite
-    redirect_to post_path(post)
+    render 'public/favorites/button_toggle'
   end
-
 end
--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,16 +1,16 @@
 class Public::PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @post_comment = current_user.post_comments.new(post_comment_params)
+    @post_comment.post_id = @post.id
+    @post_comment.save
   end
 
   def destroy
-    PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post_comment = PostComment.find(params[:id])
+    @post = @post_comment.post
+    @post_comment.destroy
   end
 
   private
